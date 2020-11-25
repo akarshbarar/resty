7107a3a41fefd98309eba32422bf42f674ea03c2
Akarsh Barar <barar.akarsh8@gmail.com>
2020-11-26 03:15:06 +0530

    Configuration done:

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,5 +47,11 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  },
+  tailwindcss: {
+    cssPath: './assets/css/tailwind.css',
+    configPath: './tailwind.config.js',
+    exposeConfig: false,
+    config: {}
   }
 }
